a3fcee71b09e7a46607526e8b382210d299783e3
unknown <rhnabil.aiub@gmail.com>
2022-11-16 12:25:22 +0600

    3. email service

--- resources/views/pages/customer/myorders.blade.php
+++ resources/views/pages/customer/myorders.blade.php
@@ -5,14 +5,14 @@
             <th>Id</th>
             <th>Status</th>
             <th>Price</th>
-            <th>Details</th>
+            <th colspan=2>Details</th>
         </tr>
         @foreach($orders as $item)
             <tr>
                 <td><a href="{{route('customer.myorders.details',['id'=>$item->id])}}">{{$item->id}}</a></td>
                 <td>{{$item->status}}</td>
                 <td>{{$item->price}}</td>
-                <td><a href="{{route('customer.myorders.details',['id'=>$item->id])}}" class="btn btn-info">Details</a></td>
+                <td><a href="{{route('customer.myorders.invoiceEmail',['id'=>$item->id])}}" class="btn btn-info">Send Invoice</a></td>
             </tr>
         @endforeach
     </table>
